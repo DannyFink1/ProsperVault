1d9767cd76a41a3f81870c71d748886d16053b78
Florian <florian.gugele@gmail.com>
2024-05-16 14:48:14 +0200

    Header weitergemacht

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -3,10 +3,18 @@ import React from 'react'
 export default function HeaderComponent() {
   return (
     <div className='h-[150px] bg-[#FBF315]'>
-        <div className='text-[40px] font-extrabold'>
-            Prospervault
-            Bild
-        </div>
+      <div className='text-[40px] font-extrabold border-b-4 border-black flex justify-between p-2'>
+        <p>Prospervault Bild</p>
+        <p>Suchleiste</p>
+        <p>Profil</p>
+      </div>
+      <div className='border-t-4 border-black flex justify-between p-5'>
+        <p>Anmelden</p>
+        <p> Online Banking</p>
+        <p>Unsere Geschichte</p>
+        <p> Börse</p>
+        <p> Impressum</p>
+      </div>
     </div>
   )
 }
