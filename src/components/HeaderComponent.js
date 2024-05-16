b60bdb7c0d727b855871f2bfc59791b3958893c2
DannyFink1 <114002047+DannyFink1@users.noreply.github.com>
2024-05-16 14:06:50 +0200

    refactoring RAN

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HeaderComponent() {
+  return (
+    <div className='h-[150px] bg-[#FBF315]'>
+        <div className='text-[40px] font-extrabold'>
+            Prospervault
+            Bild
+        </div>
+    </div>
+  )
+}
